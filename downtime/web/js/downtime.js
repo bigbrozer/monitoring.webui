d8a45b75194ff4adb73bd8ca662e765a2d3ccb5d
Vincent BESANCON <besancon.vincent@gmail.com>
2011-02-10 14:01:32 +0100

    Hide Start and End period inputs if we are defining a recurrent downtime.

--- downtime/web/js/downtime.js
+++ downtime/web/js/downtime.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
 		$('#recurrent_options input[type=checkbox]').attr('checked', false);
 		$('#recurrent_options #start_hour').attr('value', "");
 		$('#recurrent_options #end_hour').attr('value', "");
+		
+		// Disable Start and End period input if recurrent
+		$("#period_box").toggle();
+		$('#start_period').attr('value', "");
+		$('#end_period').attr('value', "");
 	});
 });
