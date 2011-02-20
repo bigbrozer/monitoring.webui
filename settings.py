0ac281bb0457a0791211b6c336b85e3c2c5c838f
Vincent BESANCON <besancon.vincent@gmail.com>
2011-02-20 18:11:19 +0100

    Created master database storing users, permissions...

--- settings.py
+++ settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db/maintenance.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', 		# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db/master.db',							# Or path to database file if using sqlite3.
+        'USER': '',                      				# Not used with sqlite3.
+        'PASSWORD': '',                  				# Not used with sqlite3.
+        'HOST': '',                      				# Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      				# Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -91,7 +91,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
