34f82a8f963263bd236a373d1a44035b54c6fcc3
Vincent BESANCON <besancon.vincent@gmail.com>
2011-02-20 17:55:42 +0100

    Created schedule view for downtime app and allow static content to be served locally if DEBUG mode is on

--- downtime/views.py
+++ downtime/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+# Downtime app views
+
+from django.shortcuts import render_to_response
+
+def schedule(request):
+	title = 'Schedule a downtime in Nagios'
+	return render_to_response('downtime/schedule_downtime.html', {'title': title})
