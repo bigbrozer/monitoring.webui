d69a1a3153d48f0981d017b8b0b79528fac5c143
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-03 00:31:20 +0100

    Use os module to make sure templates can be found whatever location of project directory is.

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'templates',
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -98,4 +98,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    
+    # Reporting
+	'optools.Reporting',
 )
