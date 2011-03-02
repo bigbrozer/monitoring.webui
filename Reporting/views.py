5141deb2fa44c23d14a5f9d87da22ac53d7864ae
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-03 00:33:55 +0100

    Update Reporting.status_data view so that we use model AckStat to get values

--- Reporting/views.py
+++ Reporting/views.py
@@ -4,11 +4,15 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
+# Models
+from optools.Reporting.models import AckStat
+
 # Open Flash Chart imports
 import openFlashChart
 from openFlashChart_varieties import (Bar, x_axis_labels)
 
-from datetime import datetime
+# Utility
+import math
 
 # The view that show graph about status
 def status(request):
@@ -17,23 +21,32 @@ def status(request):
 
 # Create graph data for status, return json data
 def status_data(request):
-	# Date calculation
-	now = datetime.now()
-	current_year = now.year
-	current_week = now.isocalendar()[1]
+	# Some var used in this view
 	weeks = []
-	for i in range(5):
-		if current_week == 1:
-			current_week = 53
-			current_year -= 1
-		current_week -= 1
-		weeks.insert(0, 'Week {0!s} - {1!s}'.format(current_week, current_year))
+	warning_bar_values = []
+	critical_bar_values = []
+	y_max_limit = 10
+	
+	# Query DB to get values of last 5 entries
+	for stat in AckStat.objects.order_by('-date')[0:5]:
+		week = stat.date.isocalendar()[1]
+		year = stat.date.isocalendar()[0]
+		
+		weeks.insert(0, 'Week {0!s} - {1!s}'.format(week, year))
+		warning_bar_values.insert(0, stat.active_ack_warn)
+		critical_bar_values.insert(0, stat.active_ack_crit)
+	
+	# Compute MAX value that could be in graph for Y axis limit
+	if max(warning_bar_values) > max(critical_bar_values):
+		y_max_limit = math.ceil(max(warning_bar_values) / 10.) * 10
+	else:
+		y_max_limit = math.ceil(max(critical_bar_values) / 10.) * 10
 	
 	# Graphs creation
-	warning = Bar(text = 'Warning', values = range(6, 1, -1))
+	warning = Bar(text = 'Warning', values = warning_bar_values)
 	warning.set_colour('#DDDD00')
 	warning.set_tooltip(r'Warning<br>Value:#val#')
-	critical = Bar(text = 'Critical', values = range(1, 6, 1))
+	critical = Bar(text = 'Critical', values = critical_bar_values)
 	critical.set_colour('#FF0000')
 	critical.set_tooltip(r'Critical<br>Value:#val#')
 
@@ -41,5 +54,7 @@ def status_data(request):
 	chart.add_element(warning)
 	chart.add_element(critical)
 	chart.set_x_axis(labels = x_axis_labels(labels = weeks))
+	chart.set_y_axis(max = y_max_limit, steps = 5)
+	chart.set_bg_colour('#FFFFFF')
 
 	return HttpResponse(chart.encode())
