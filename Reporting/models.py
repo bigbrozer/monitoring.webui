98481336fd88c00dad7305a9f1d2f82333e76d79
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-03 00:33:18 +0100

    Added AckStat model that define table storing data about ack alerts

--- Reporting/models.py
+++ Reporting/models.py
@@ -1,3 +1,12 @@
+# Reporting models
+
 from django.db import models
 
-# Create your models here.
+# Model representing stats about number of active ack alerts per weeks
+class AckStat(models.Model):
+	date = models.DateField()
+	active_ack_warn = models.PositiveIntegerField('Ack Warning alerts')
+	active_ack_crit = models.PositiveIntegerField('Ack Critical alerts')
+	
+	def __unicode__(self):
+		return u'Stat for date {0}'.format(self.date)
