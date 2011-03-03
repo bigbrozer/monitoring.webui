78be8fd25c94df0ac68b5c4c8de23ba853f1dbb0
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-03 11:32:37 +0100

    Register model ProcedureStat in the admin site

--- Reporting/admin.py
+++ Reporting/admin.py
@@ -1,9 +1,13 @@
 # Adding models to Admin site for reporting app
 
 from django.contrib import admin
-from optools.Reporting.models import AckStat
+from optools.Reporting.models import AckStat, ProcedureStat
 
 class AckStatAdmin(admin.ModelAdmin):
 	list_display = ('date', 'active_ack_warn', 'active_ack_crit')
 
+class ProcedureStatAdmin(admin.ModelAdmin):
+	list_display = ('date', 'num_no_procedure', 'num_with_procedure')
+
 admin.site.register(AckStat, AckStatAdmin)
+admin.site.register(ProcedureStat, ProcedureStatAdmin)
