99a594a006a070ec3d32244e6f91820826e866cc
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-04 16:16:04 +0100

    Testing; script to get the top 5 oldest ack alert is ready

--- scripts/report_top5_old_ack_alerts.py
+++ scripts/report_top5_old_ack_alerts.py
@@ -20,6 +20,8 @@
 #===============================================================================
 
 import os, sys
+from datetime import datetime
+from operator import attrgetter
 
 # Adding optools project to sys.path
 pwd = os.path.abspath(os.path.dirname(__file__))
@@ -28,5 +30,39 @@ sys.path.append(optools_dir)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 # Importing models
-from Reporting.models import AckStat
+from Nagios.models import Satellite
+import backends.livestatus as live
+
+# Class that define an alert result
+class Alert(object):
+	def __init__(self, host, service, state, timestamp):
+		self.host = host
+		self.service = service
+		self.state = state
+		self.date = datetime.fromtimestamp(timestamp)
+
+	def __repr__(self):
+		return repr((self.host, self.service, self.state, str(self.date)))
+
+# Connection settings to access satellites using livestatus
+satellite_connect_settings = {}
+for sat in Satellite.objects.all():
+	satellite_connect_settings.update(sat.as_live_dict())
+
+satellites = live.MultiSiteConnection(satellite_connect_settings)
+
+# Query all ack alerts
+results = satellites.query("""GET services\n\
+Columns: host_name description state last_state_change\n\
+Filter: acknowledged = 1\n""")
+
+alert_objects = []
+for result in results:
+	host_name, service, state, timestamp = result
+	
+	alert = Alert(host_name, service, state, timestamp)
+	alert_objects.append(alert)
+
+# Print the oldest top 5 ack alerts
+print sorted(alert_objects, key=attrgetter('date'))[0:5]
 
