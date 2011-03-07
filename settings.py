a4fbe9551bbd1d9d79806c7a73bef11d32a24250
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-07 09:57:31 +0100

    Added configuration for production MySQL DB

--- settings.py
+++ settings.py
@@ -11,10 +11,20 @@ ADMINS = (
     ('Vincent BESANCON', 'vincent.besancon@faurecia.com'),
 )
 
-MANAGERS = ADMINS
+MANAGERS = (
+	('Mohamed CHERROUD', 'mohamed.cherroud-ext@faurecia.com'),
+)
 
 DATABASES = {
-    'default': {
+	'default': {
+        'ENGINE': 'django.db.backends.mysql', 		# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'optools',							# Or path to database file if using sqlite3.
+        'USER': 'optools',                      				# Not used with sqlite3.
+        'PASSWORD': '62SBU3pNJnVVBA5D',                  				# Not used with sqlite3.
+        'HOST': 'monitoring-dc.app.corp',                      				# Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      				# Set to empty string for default. Not used with sqlite3.
+    },
+    'test': {
         'ENGINE': 'django.db.backends.sqlite3', 		# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(PROJECT_PATH, 'db/master.db'),							# Or path to database file if using sqlite3.
         'USER': '',                      				# Not used with sqlite3.
