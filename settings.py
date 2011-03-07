a1897598820fb0d387ab8af6d354dc2576ce8ba2
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-07 10:23:00 +0100

    Set port to access prod DB

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ DATABASES = {
         'USER': 'optools',                      				# Not used with sqlite3.
         'PASSWORD': '62SBU3pNJnVVBA5D',                  				# Not used with sqlite3.
         'HOST': 'wwgrpmon0001.ww.corp',                      				# Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      				# Set to empty string for default. Not used with sqlite3.
+        'PORT': '3306',                      				# Set to empty string for default. Not used with sqlite3.
     },
     'test': {
         'ENGINE': 'django.db.backends.sqlite3', 		# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
