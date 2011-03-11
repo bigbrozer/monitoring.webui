a1e5fe5a964924382dffcf6922d21a68daba3c3f
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-11 09:19:32 +0100

    Part I: use threads in script update_procedure_stats.py, max is configurable. (reverse-merged from commit 74822d854f328f9ad010f9a415a1bef96c18e12e)

--- scripts/update_procedure_stats.py
+++ scripts/update_procedure_stats.py
@@ -21,7 +21,6 @@
 
 import sys, os
 import urllib2
-import thread
 
 # Adding optools project to sys.path
 optools_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -40,38 +39,17 @@ for proxyvar in ('http_proxy', 'https_proxy', 'ftp_proxy'):
 	except KeyError:
 		pass
 
-# Global vars intialization
-#
-num_with_proc = 0
-num_without_proc = 0
-running_threads = 0
-max_threads = 10										# Maximun number of threads allowed to run in parallel
-csv_export_dir = '/tmp/django/public_html/reports'		# Used for testing
-#csv_export_dir = '/home/django/public_html/reports'	# The location of report (CSV) about list of services that does not have a procedure
+#csv_export_dir = '/tmp/django/public_html/reports'
+csv_export_dir = '/home/django/public_html/reports'
 if not os.path.isdir(csv_export_dir):
 	os.makedirs(csv_export_dir)
 
-# Method to get the procedure (raw format)
 def get_raw_procedure(url):
-	"""This method will retrieve the procedure as raw text, check if procedure exist or not, if not report in CSV file"""
-	global num_without_proc, num_with_proc
-	
 	try:
 		procedure = urllib2.urlopen(url + "?do=export_raw", None, 10)
 	except urllib2.HTTPError as e:
 		raise Exception("Error: URL: {0}\nMessage: {1}".format(url, e))
-		
-	if '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0' in procedure.read():
-		num_without_proc += 1
-		# Reserve write access to the CSV file for this thread
-		mutex.acquire()
-		output_csv.write("{0};{1};{2};{3}\n".format(host, service, kb_url, ','.join(contacts)))
-		mutex.release()		# Release lock
-	else:
-		num_with_proc += 1
-	
-	# End of thread execution
-	running_threads -= 1
+	return procedure.read()
 
 # Connection settings to access satellites using livestatus
 satellite_connect_settings = {}
@@ -93,24 +71,23 @@ output_csv.write('host_name;service;url\n')
 # Check procedures
 total_services = len(results)
 progress = 1
-mutex = thread.allocate_lock()
+num_with_proc = 0
+num_without_proc = 0
 for service_object in results:
 	host, service, kb_url, contacts = service_object
 	#print "Checking procedure: {0}/{1}.".format(progress, total_services)
-	if running_threads < max_threads:
-		running_threads += 1
-		thread.start_new_thread(get_raw_procedure, (kb_url,))
+	procedure = get_raw_procedure(kb_url)
+	if '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0' in procedure:
+		num_without_proc += 1
+		output_csv.write("{0};{1};{2};{3}\n".format(host, service, kb_url, ','.join(contacts)))
 	else:
-		while running_threads >= max_threads: pass
+		num_with_proc += 1
 	progress+=1
-
-# Wait for still running threads
-while running_threads: pass
 
 # Close CSV file
 output_csv.close()
 
 # Send results to database
-#stat = ProcedureStat(num_no_procedure = num_without_proc, num_with_procedure = num_with_proc)
-#stat.save()
+stat = ProcedureStat(num_no_procedure = num_without_proc, num_with_procedure = num_with_proc)
+stat.save()
 
