1df6ef796ace53459a799989ab0cfe5564570bb3
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-18 11:52:37 +0100

    Downtime: added initial data for start period, raise an error is end period is defined in the past

--- apps/downtime/forms.py
+++ apps/downtime/forms.py
@@ -2,12 +2,16 @@
 
 from django import forms
 
+# Utilities
+from datetime import datetime
+
 # Schedule a downtime form
 class ScheduleDowntimeForm(forms.Form):
 	downtime_name = forms.CharField(label='Downtime name', help_text='Name of downtime, ex: PDM Backup', max_length=100)
 	search_host = forms.CharField(label='Search host', help_text='Search host to schedule', required=False)
 	host_list = forms.CharField(label='Host name list', help_text='Enter host names separated by comma', widget=forms.Textarea)
-	start_period = forms.DateTimeField(label='Start period', help_text='Start date for the downtime', required=False)
+	start_period = forms.DateTimeField(label='Start period', help_text='Start date for the downtime',
+		required=False, initial=datetime.strftime(datetime.now(), '%m/%d/%Y %H:%M'))
 	end_period = forms.DateTimeField(label='End period', help_text='End date for the downtime', required=False)
 	is_recurrent = forms.BooleanField(label='Recurrent', help_text='Is it a recurrent downtime ?', required=False)
 	
@@ -59,6 +63,10 @@ class ScheduleDowntimeForm(forms.Form):
 			# Check that end period is ALWAYS highter than start period
 			if start_period > end_period:
 				raise forms.ValidationError('Start period should be defined before end period.')
+			
+			# Check that end period is not defined in the past
+			if end_period <= datetime.now():
+				raise forms.ValidationError('End period should be defined in the future.')
 		
 		# Recurrent downtime
 		else:
