f11c8c1521f597acb2055df8abfa24b1ceeb9d20
Vincent BESANCON <besancon.vincent@gmail.com>
2011-03-31 17:52:33 +0200

    Reporting: register model NagiosKPI to the admin site

--- apps/reporting/admin.py
+++ apps/reporting/admin.py
@@ -1,7 +1,7 @@
 # Adding models to Admin site for reporting app
 
 from django.contrib import admin
-from optools.apps.reporting.models import AckStat, ProcedureStat
+from optools.apps.reporting.models import AckStat, ProcedureStat, NagiosKPI
 
 class AckStatAdmin(admin.ModelAdmin):
 	list_display = ('date', 'active_ack_warn', 'active_ack_crit')
@@ -9,5 +9,9 @@ class AckStatAdmin(admin.ModelAdmin):
 class ProcedureStatAdmin(admin.ModelAdmin):
 	list_display = ('date', 'num_no_procedure', 'num_with_procedure')
 
+class NagiosKPIAdmin(admin.ModelAdmin):
+	list_display = ('date', 'total_hosts', 'total_services', 'alert_warn_total', 'alert_crit_total', 'service_without_kb')
+
 admin.site.register(AckStat, AckStatAdmin)
 admin.site.register(ProcedureStat, ProcedureStatAdmin)
+admin.site.register(NagiosKPI, NagiosKPIAdmin)
