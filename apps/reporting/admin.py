d7a7ad3e1262c582f20704fd38e33074f3e993a0
Vincent BESANCON <besancon.vincent@gmail.com>
2011-04-01 14:48:08 +0200

    Reporting: updated models, use now only one for all Nagios kpi.

--- apps/reporting/admin.py
+++ apps/reporting/admin.py
@@ -1,17 +1,9 @@
 # Adding models to Admin site for reporting app
 
 from django.contrib import admin
-from optools.apps.reporting.models import AckStat, ProcedureStat, NagiosKPI
-
-class AckStatAdmin(admin.ModelAdmin):
-	list_display = ('date', 'active_ack_warn', 'active_ack_crit')
-
-class ProcedureStatAdmin(admin.ModelAdmin):
-	list_display = ('date', 'num_no_procedure', 'num_with_procedure')
+from optools.apps.reporting.models import NagiosKPI
 
 class NagiosKPIAdmin(admin.ModelAdmin):
 	list_display = ('date', 'total_hosts', 'total_services', 'alert_warn_total', 'alert_crit_total', 'service_without_kb')
 
-admin.site.register(AckStat, AckStatAdmin)
-admin.site.register(ProcedureStat, ProcedureStatAdmin)
 admin.site.register(NagiosKPI, NagiosKPIAdmin)
--- apps/reporting/models.py
+++ apps/reporting/models.py
@@ -3,49 +3,21 @@
 from django.db import models
 
 # Utility
-from datetime import date
-
-#===============================================================================
-#  ____                                _           _ 
-# |  _ \  ___ _ __  _ __ ___  ___ __ _| |_ ___  __| |
-# | | | |/ _ \ '_ \| '__/ _ \/ __/ _` | __/ _ \/ _` |
-# | |_| |  __/ |_) | | |  __/ (_| (_| | ||  __/ (_| |
-# |____/ \___| .__/|_|  \___|\___\__,_|\__\___|\__,_|
-#            |_|                                     
-#
-
-# Model representing stats about number of active ack alerts per weeks
-class AckStat(models.Model):
-	date = models.DateField(auto_now_add=True)
-	active_ack_warn = models.PositiveIntegerField('Ack Warning alerts')
-	active_ack_crit = models.PositiveIntegerField('Ack Critical alerts')
-	
-	def __unicode__(self):
-		return u'Stat for date {0}'.format(self.date)
-
-# Model representing stats about number of procedures
-class ProcedureStat(models.Model):
-	date = models.DateField(auto_now_add=True, auto_now=True)
-	num_no_procedure = models.PositiveIntegerField()
-	num_with_procedure = models.PositiveIntegerField()
-	
-	def __unicode__(self):
-		return u'{0} services without and {1} with procedure for date {2}'.format(self.num_no_procedure, self.num_with_procedure, self.date)
-#===============================================================================
+from datetime import datetime
 
 # Nagios KPI model
 class NagiosKPI (models.Model):
-	date = models.DateField(auto_now_add=True)
-	service_with_kb = models.PositiveIntegerField()
-	service_without_kb = models.PositiveIntegerField()
-	alert_warn_total = models.PositiveIntegerField()
-	alert_crit_total = models.PositiveIntegerField()
-	alert_ack_warn_total = models.PositiveIntegerField()
-	alert_ack_crit_total = models.PositiveIntegerField()
-	alert_ack_warn_current = models.PositiveIntegerField()
-	alert_ack_crit_current = models.PositiveIntegerField()
-	total_hosts = models.PositiveIntegerField()
-	total_services = models.PositiveIntegerField()
+	date = models.DateTimeField(auto_now_add=True)
+	service_with_kb = models.PositiveIntegerField(default=0)
+	service_without_kb = models.PositiveIntegerField(default=0)
+	alert_warn_total = models.PositiveIntegerField(default=0)
+	alert_crit_total = models.PositiveIntegerField(default=0)
+	alert_ack_warn_total = models.PositiveIntegerField(default=0)
+	alert_ack_crit_total = models.PositiveIntegerField(default=0)
+	alert_ack_warn_current = models.PositiveIntegerField(default=0)
+	alert_ack_crit_current = models.PositiveIntegerField(default=0)
+	total_hosts = models.PositiveIntegerField(default=0)
+	total_services = models.PositiveIntegerField(default=0)
 	
 	def __unicode__(self):
-		return u'Nagios KPI ({0})'.format(self.date)
+		return u'Nagios KPI ({0})'.format(self.date.strftime('%d-%m-%Y %H:%M:%S'))
