38e4f24fda559edcf347781389c1339640581685
Vincent BESANCON <besancon.vincent@gmail.com>
2011-04-01 14:49:27 +0200

    Scripts: created update_nagios_kpi.py script to get all Nagios KPI.

--- scripts/update_nagios_kpi.py
+++ scripts/update_nagios_kpi.py
@@ -25,6 +25,8 @@
 # 1 week == 604800 seconds
 
 import os, sys
+import time
+from datetime import date
 
 # Adding optools project to sys.path
 optools_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -38,6 +40,19 @@ from optools.apps.reporting.models import NagiosKPI
 # External backends
 import optools.backends.livestatus as live
 
+# Exceptions
+from optools.lib.exceptions import NoSatellites
+
+# Utilities functions
+def combine_stats_result(result_data):
+	'''Combine stats results for all satellites into a big array'''
+	limit = len(result_data[0])
+	results = [0] * limit
+	for i in xrange(limit):
+		for data in result_data:
+			results[i] += data[i]
+	return results
+
 # Connection settings to access satellites using livestatus
 satellite_connect_settings = {}
 for sat in Satellite.objects.all():
@@ -45,14 +60,95 @@ for sat in Satellite.objects.all():
 
 satellites = live.MultiSiteConnection(satellite_connect_settings)
 if satellites.dead_sites():
-	raise Exception('Unable to get data from Nagios satellites !')
+	raise NoSatellites()
 
-# Query all warn alerts
-results = satellites.query("""GET log\n\
-Columns: message\n\
-Filter: time >= 1300984164\n\
+satellites = live.MultiSiteConnection(satellite_connect_settings)
+#satellites.set_prepend_site(True)
+
+# Today timestamp
+today_timestamp = time.time()
+
+# Get the last KPI from database
+# Take into account if no KPI are existing in db
+try:
+	last_kpi = NagiosKPI.objects.order_by('-date')[0]
+	log_startfrom = time.mktime(last_kpi.date.timetuple())	# Convert date object to timestamp, used in querying nagios log
+except IndexError:
+	# If no KPI are existing, start querying log one week before
+	log_startfrom = today_timestamp - 604800.
+
+# Query for all critical & warning alerts (including host alerts as critical)
+# Results: [Crit, Warn, AckedCrit, AckedWarn]
+query_stats_alerts = """GET log\n\
+Columns: type state\n\
+Filter: time >= {0:.0f}\n\
+Filter: class = 3\n\
 Filter: state = 1\n\
-Filter: options ~ ACKNOWLEDGEMENT\n\
-Filter: class = 3\n""")
+Filter: state = 2\n\
+Or: 2\n\
+Stats: type ~ ^HOST\n\
+Stats: state = 2\n\
+StatsOr: 2\n\
+Stats: type ~ ^SERVICE\n\
+Stats: state = 1\n\
+StatsAnd: 2\n\
+Stats: options ~ ACKNOWLEDGEMENT\n\
+Stats: state = 2\n\
+StatsAnd: 2\n\
+Stats: options ~ ACKNOWLEDGEMENT\n\
+Stats: state = 1\n\
+StatsAnd: 2\n""".format(log_startfrom)
 
-print results
+stats_alerts = combine_stats_result(satellites.query(query_stats_alerts))
+
+# Query for current acknowledged alerts
+# Results: [AckCrit, AckWarn]
+query_current_svc_acknowledged = """GET services\n\
+Columns: description state acknowledged\n\
+Filter: acknowledged = 1\n\
+Filter: state = 2\n\
+Filter: state = 1\n\
+Or: 2\n\
+Stats: state = 2\n\
+Stats: state = 1\n"""
+
+# Results: [AckHosts]
+query_current_host_acknowledged = """GET hosts\n\
+Columns: name state acknowledged\n\
+Filter: acknowledged = 1\n\
+Stats: state = 1\n\
+Stats: state = 2\n\
+StatsOr: 2\n"""
+
+current_acknowledged = combine_stats_result(satellites.query(query_current_svc_acknowledged))
+current_host_acknowledged = combine_stats_result(satellites.query(query_current_host_acknowledged))
+current_acknowledged[0] += current_host_acknowledged.pop()		# Host alerts are considered as critical
+
+# Query for total number of monitored hosts
+# Results: [TotalHosts]
+query_total_monitored_hosts = """GET hosts\n\
+Columns: name\n\
+Stats: name !~ \" \"\n"""
+
+total_monitored_hosts = combine_stats_result(satellites.query(query_total_monitored_hosts))
+
+# Query for total number of monitored services
+# Results: [TotalServices]
+query_total_monitored_services = """GET services\n\
+Columns: description\n\
+Stats: description !~ \" \"\n"""
+
+total_monitored_services = combine_stats_result(satellites.query(query_total_monitored_services))
+
+# Save KPI in database
+kpi = NagiosKPI()
+kpi.alert_crit_total = stats_alerts[0]
+kpi.alert_warn_total = stats_alerts[1]
+kpi.alert_ack_crit_total = stats_alerts[2]
+kpi.alert_ack_warn_total = stats_alerts[3]
+kpi.alert_ack_crit_current = current_acknowledged[0]
+kpi.alert_ack_warn_current = current_acknowledged[1]
+kpi.total_hosts = total_monitored_hosts[0]
+kpi.total_services = total_monitored_services[0]
+kpi.save()
+
