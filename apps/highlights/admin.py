7e11fcfe86506f031c6fab8f7f487e8ba0c8e80b
Vincent BESANCON <besancon.vincent@gmail.com>
2011-04-04 10:05:08 +0200

    Highlights: add export to csv action to the admin site

--- apps/highlights/admin.py
+++ apps/highlights/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 from optools.apps.highlights.models import Project, Highlight
+from optools.apps.highlights.actions import export_as_csv_action
 
 class ProjectAdmin(admin.ModelAdmin):
 	list_display = ('name',)
@@ -12,6 +13,14 @@ class HighlightAdmin(admin.ModelAdmin):
 	search_fields = ('title', 'description')
 	date_hierarchy = 'date'
 	ordering = ('-date',)
+	actions = [
+		export_as_csv_action("Export selected highlights as Excel file", fields=[
+			'date',
+			'title',
+			'project',
+			'description',
+		], header=True),
+	]
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Highlight, HighlightAdmin)
