224607fd70b1b041de7b42b420cc9105596b8390
Vincent BESANCON <besancon.vincent@gmail.com>
2012-08-01 11:42:19 +0200

    Fixed now the redirect after editing User profile.

--- forms.py
+++ forms.py
@@ -1,13 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext, ugettext_lazy as _
 
 class UserEditForm(forms.ModelForm):
     """
-    CLass based view to show the User profile editing form.
+    Customize the User profile editing form based on User model.
     """
-    success_url = "/optools/accounts/profile"
-
     class Meta:
         model = User
         fields = ('first_name', 'last_name')
