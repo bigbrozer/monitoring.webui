27942c0feccd211f2c2447add077ea093e381e81
Vincent BESANCON <besancon.vincent@gmail.com>
2012-08-02 10:50:27 +0200

    Show email in user profile.

--- apps/common/forms.py
+++ apps/common/forms.py
@@ -7,5 +7,5 @@ class UserEditForm(forms.ModelForm):
     """
     class Meta:
         model = User
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'email')
 
