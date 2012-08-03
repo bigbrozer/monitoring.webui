c77bae93665399cec39b0caa635d521321e6eeec
Vincent BESANCON <besancon.vincent@gmail.com>
2012-08-03 02:17:40 +0200

    Added a view for not supported browser.

--- views.py
+++ views.py
@@ -1,11 +1,15 @@
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.views.generic import UpdateView
+
+import httpagentparser
 
 from apps.common.forms import UserEditForm
 
 class UserEdit(UpdateView):
     """
-    CLass based view to show the User profile editing form.
+    Class based view to show the User profile editing form.
     """
     form_class = UserEditForm
     model = User
@@ -16,3 +20,16 @@ class UserEdit(UpdateView):
 
     def get_success_url(self):
         return "/optools/accounts/profile/"
+
+def browser_out_of_date(request):
+    """
+    Warn user that the browser is not well supported.
+    """
+    title = "Browser out of date"
+
+    browser = httpagentparser.detect(request.META['HTTP_USER_AGENT'])['browser']
+    if "internet explorer" in browser['name'].lower() and int(browser['version'].split('.')[0]) < 9:
+        return render_to_response("common/browser_not_supported.html", locals(), context_instance = RequestContext(request))
+    else:
+        return redirect('portal_home')
+
