62a4ca0e72b9be8098e1030e5bb2dc13d5d4cce6
Vincent BESANCON <besancon.vincent@gmail.com>
2012-08-07 14:36:35 +0200

    Make submit buttons in forms to wait until form is submited.

--- static/js/optools.js
+++ static/js/optools.js
@@ -4,10 +4,16 @@
 function initialize_bootstrap() {
     $("[rel=tooltip]").tooltip();
     $("[rel=popover]").popover();
+    $("button").button();
 }
 
 /* Events when document is ready */
 $(document).ready(function() {
     initialize_bootstrap();
+
+    // Make submit buttons in forms in waiting mode
+    $("button[type=submit]").click(function() {
+        $(this).button('loading');
+    });
 });
 
