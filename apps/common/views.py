d7e29d0a536b0ea2d88b24a5ba8a097faa4c969c
Vincent BESANCON <besancon.vincent@gmail.com>
2012-08-07 11:27:32 +0200

    Make login() view redirect to next get var or portal home if not set.

--- apps/common/views.py
+++ apps/common/views.py
@@ -4,20 +4,29 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.views.generic import UpdateView
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 import httpagentparser
 
 from apps.common.forms import UserEditForm
 
-@login_required
 def login(request):
     """
     Called after successfull basic HTTP authentication and check if user filled
     his profile.
     """
-    user = User.objects.get(username=request.META['REMOTE_USER'])
+    # Find user
+    if settings.DEBUG:
+        user = User.objects.get(username='besancon@corp')
+    else:
+        user = User.objects.get(username=request.META['REMOTE_USER'])
+
+    # Test if user filled his profile
     if user.first_name and user.last_name and user.email:
-        return redirect('portal_home')
+        if request.GET.has_key('next'):
+            return redirect(request.GET['next'])
+        else:
+            return redirect('portal_home')
     else:
         return redirect('user_profile')
 
