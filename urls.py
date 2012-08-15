b472a16ed4be23b91d3366dd2633e710c3d5c002
Vincent BESANCON <besancon.vincent@gmail.com>
2012-08-15 02:05:07 +0200

    Show announcement after successfull login.
    - Use app.label.urls for app's urls.

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+from views import UserEdit
+
+urlpatterns = patterns('apps.common.views',
+    # Login / Logout
+    url(r'^accounts/login/$', 'http_login', name='login'),
+    url(r'^accounts/profile/$', login_required(UserEdit.as_view()), name='user_profile'),
+
+    # Misc
+    url(r'^support/browser/$', 'browser_out_of_date', name='browser_out_of_date'),
+    (r'^maintenance/$', TemplateView.as_view(template_name='maintenance.html')),
+    (r'^error/404/$', TemplateView.as_view(template_name='404.html')),
+    (r'^error/500/$', TemplateView.as_view(template_name='500.html')),
+)
+
