8d0ee9fdc3e58709a4b5ed55ba28fd8cf05f754a
Vincent BESANCON <besancon.vincent@gmail.com>
2012-09-06 17:23:12 +0200

    Init find_procedure view.

--- apps/nagios/views.py
+++ apps/nagios/views.py
@@ -7,19 +7,28 @@ from django.core import serializers
 # Models imports
 from apps.nagios.models import Satellite
 
-# Utility
-import cjson
 
 # View definitions
 # ================
 #
-# Return the list of all satellites, format is json by default.
 def get_satellite_list(request, format='json'):
+    """
+    Return the list of all satellites, format is json by default.
+    """
     if format not in "csv":
-    	return HttpResponse(serializers.serialize(format, Satellite.objects.all()))
+        return HttpResponse(serializers.serialize(format, Satellite.objects.all()))
     else:
         csv = ""
         for sat in Satellite.objects.all():
             csv += "%s;%s;%s;%s\n" % (sat.name, sat.fqdn, sat.alias, sat.live_port)
         return HttpResponse(csv)
 
+
+def find_procedure(request, wiki_url):
+    """
+    Return the first procedure page based on a Wiki link.
+
+    :param wiki_url: the full dokuwiki URL of the form ``xxx:xxx:xxx``...
+    :returns: the first wiki page found.
+    """
+    pass
