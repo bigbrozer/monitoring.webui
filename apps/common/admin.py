bfa168cb3a4dd96fcf3427b309c8cdee5ff5191d
Vincent BESANCON <besancon.vincent@gmail.com>
2012-09-18 16:53:28 +0200

    Customize User model admin. Add is_active and date_joined columns. Allow to edit tables is_active and is_staff.

---
+++ apps/common/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+# Customize the User model admin
+UserAdmin.list_display = ('username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'date_joined')
+UserAdmin.list_editable = ('is_active', 'is_staff')
+UserAdmin.ordering = ['-date_joined']
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin) 
+
