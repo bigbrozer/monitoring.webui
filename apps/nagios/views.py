ce2ffd4c6996bac8703d29c5075540b78ed355e7
Vincent BESANCON <besancon.vincent@gmail.com>
2012-10-04 17:20:42 +0200

    Add new section Satellites in navar.
    Show the list of active Nagios satellites and conf for network ports.

--- apps/nagios/views.py
+++ apps/nagios/views.py
@@ -3,12 +3,10 @@
 # Django imports
 from django.http import HttpResponse
 from django.core import serializers
+from django.views.generic import ListView
 
 # Models imports
 from apps.nagios.models import Satellite
-
-# Utility
-import cjson
 
 # View definitions
 # ================
@@ -16,10 +14,21 @@ import cjson
 # Return the list of all satellites, format is json by default.
 def get_satellite_list(request, format='json'):
     if format not in "csv":
-    	return HttpResponse(serializers.serialize(format, Satellite.objects.all()))
+        return HttpResponse(serializers.serialize(format, Satellite.objects.all()))
     else:
         csv = ""
         for sat in Satellite.objects.all():
             csv += "%s;%s;%s;%s\n" % (sat.name, sat.fqdn, sat.alias, sat.live_port)
         return HttpResponse(csv)
 
+
+# Show the list of satellites
+class SatelliteListView(ListView):
+    context_object_name = "satellite_list"
+    model = Satellite
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(SatelliteListView, self).get_context_data(**kwargs)
+        context['section'] = {'satellites': 'active'}
+        return context
