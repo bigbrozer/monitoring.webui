88526fc5ea08bbaf757e635a8ae2384704f79667
Vincent BESANCON <besancon.vincent@gmail.com>
2012-10-16 17:40:35 +0200

    Define model Procedure for kb app.
    Just setup custom permissions for the moment.

--- apps/kb/models.py
+++ apps/kb/models.py
@@ -1,3 +1,21 @@
+"""
+:mod:`kb.models` -- Models for app kb.
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Procedure(models.Model):
+    """
+    Procedure Model.
+
+    Store informations about KB in Dokuwiki.
+
+    - Namespace
+    - Rating
+    - Comment
+    """
+    class Meta:
+        permissions = (
+            ("rate_procedure", "Can rate a procedure"),
+        )
