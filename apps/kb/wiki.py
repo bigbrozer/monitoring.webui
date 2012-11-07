02fc8b47e73bbcb2ab81a3a99c4dcae3f43a459a
Vincent BESANCON <besancon.vincent@gmail.com>
2012-11-07 16:10:17 +0100

    Kb: complete rewrite of wiki module. Created a class Wiki that manipulate all KB in dokuwiki.

--- apps/kb/wiki.py
+++ apps/kb/wiki.py
@@ -1,25 +1,36 @@
 """
-:mod:`kb.wiki` to handle Alert procedures (KB)
+:mod:`apps.kb.wiki` to handle Alert procedures (KB)
 """
 
 # Std lib imports
 import os
 import re
+from datetime import datetime
+import logging
+from pprint import pformat
 
 # Django imports
 os.environ['DJANGO_SETTINGS_MODULE'] = 'optools.settings'
 from django.conf import settings
-
-
-# Import *
-__all__ = ['KbError', 'KbUrlMalformed', 'get_procedure_details', 'iterpages']
+from django.utils.timezone import utc
 
 # Dokuwiki config
 DOKUWIKI_BASE_URL = '/kb' if not settings.DEBUG else 'http://monitoring-dc.app.corp/kb'
-DOKUWIKI_DIR = '/var/www/kb/data/pages' if not settings.DEBUG else os.path.join(settings.PROJECT_PATH, 'var/pages')
+DOKUWIKI_PAGES_DIR = '/var/www/kb/data/pages' if not settings.DEBUG else os.path.join(settings.PROJECT_PATH, 'var/pages')
+DOKUWIKI_META_DIR = '/var/www/kb/data/meta' if not settings.DEBUG else os.path.join(settings.PROJECT_PATH, 'var/meta')
+
+logger = logging.getLogger('optools.debug')
+logger.setLevel(logging.INFO)
 
 
-# Exceptions
+#===============================================================================
+#  _____                    _   _
+# | ____|_  _____ ___ _ __ | |_(_) ___  _ __  ___
+# |  _| \ \/ / __/ _ \ '_ \| __| |/ _ \| '_ \/ __|
+# | |___ >  < (_|  __/ |_) | |_| | (_) | | | \__ \
+# |_____/_/\_\___\___| .__/ \__|_|\___/|_| |_|___/
+#                    |_|
+#===============================================================================
 class KbError(Exception):
     """
     Base class for all exceptions related to KB.
@@ -38,90 +49,146 @@ class KbUrlMalformed(KbError):
     Raised when given procedure URL does not validate.
     """
     def __init__(self, kb, regexp):
-        self.message = 'KB URL \"%s\" is malformed. Must be \"%s\" !' % (kb, regexp)
+        self.message = 'KB namespace \"%s\" is malformed. Must be \"%s\" !' % (kb, regexp)
         super(KbUrlMalformed, self).__init__(self.message)
 
 
-# Procedures
-def get_procedure_details(namespace):
+#===============================================================================
+#   ____ _
+#  / ___| | __ _ ___ ___  ___  ___
+# | |   | |/ _` / __/ __|/ _ \/ __|
+# | |___| | (_| \__ \__ \  __/\__ \
+#  \____|_|\__,_|___/___/\___||___/
+
+#===============================================================================
+class Kb(object):
     """
-    Return details about all KB of the specified ``namespace``. Check request #1493.
-
-    It returns a list of dict containing all info about a namespace, use the following keys:
-
-    * **created**: is the procedure is written ?
-    * **edit_url**: link to edit procedure in dokuwiki
-    * **file**: local file storing the procedure content
-    * **namespace**: dokuwiki URL for the procedure (xxx:xxx:xxx...)
-    * **page**: name of the procedure page
-
-    :param namespace: the dokuwiki's namespace of the form ``xxx:xxx:xxx[...]``.
-    :rtype: list
+    This class init a Kb and store information such as: name, namespace, last modified timestamp, parents kb, etc...
     """
+    logger = logging.getLogger('optools.debug.kb.wiki.Kb')
 
-    # Test KB URL before proceeding
-    kb_url_regexp = r'^([a-zA-Z0-9_-]+:*)+$'
-    if not re.match(kb_url_regexp, namespace):
-        raise KbUrlMalformed(namespace, kb_url_regexp)
+    namespace_valid_regexp = r'^([a-zA-Z0-9_-]+:*)+$'
 
-    kb_namespaces = namespace.lower().strip(':').split(':')
-    kb_details = []
+    def __init__(self, namespace):
+        # Validate namespace before proceeding
+        if not re.match(Kb.namespace_valid_regexp, namespace):
+            raise KbUrlMalformed(namespace, Kb.namespace_valid_regexp)
 
-    # Browse all namespaces levels and get info on each namespaces
-    for pos, nsp in enumerate(kb_namespaces):
-        pos += 1
-        kb_info = {}
+        # Attributes
+        self.namespace = namespace.lower().strip(':')
+        self.name = self.namespace.split(':')[-1]
+        self.filename = os.path.join(DOKUWIKI_PAGES_DIR, "{}.txt".format(self.namespace.replace(':', '/')))
+        self.changes_filename = os.path.join(DOKUWIKI_META_DIR, "{}.changes".format(self.namespace.replace(':', '/')))
+        self.is_written = False
+        self.META = {
+            'read_url': '{}/{}'.format(DOKUWIKI_BASE_URL, self.namespace),
+            'edit_url': '{}/{}?do=edit'.format(DOKUWIKI_BASE_URL, self.namespace),
+        }
+        self.parents = []
 
-        kb_info['page'] = nsp
-        kb_info['namespace'] = ':'.join(kb_namespaces[:pos])
-        kb_info['file'] = os.path.join(DOKUWIKI_DIR, "{0}.txt".format('/'.join(kb_namespaces[:pos])))
-        kb_info['read_url'] = '{0}/{1}'.format(DOKUWIKI_BASE_URL, kb_info['namespace'])
-        kb_info['edit_url'] = '{0}?do=edit'.format(kb_info['read_url'])
+        if os.path.isfile(self.filename):
+            self.is_written = True
 
-        if os.path.isfile(kb_info['file']):
-            kb_info['created'] = True
+        # Building parents and META
+        self._populate_meta()
+        self._build_parents_list()
+        self.logger.debug('%s attributes:\n%s', repr(self), pformat(self.__dict__))
+
+    def _build_parents_list(self):
+        parents_pages = self.namespace.split(':')[:-1]
+        for index, parent in enumerate(parents_pages):
+            index += 1
+            self.parents.append(":".join(parents_pages[:index]))
+
+    def _populate_meta(self):
+        """Populate META information about the Kb."""
+        try:
+            with open(self.changes_filename, 'r') as meta_file:
+                last_change = meta_file.readlines()[-1].split('\t')
+                self.logger.debug("Parsing line:\n%s", pformat(last_change))
+                self.META['last_modified'] = datetime.fromtimestamp(long(last_change[0]), tz=utc)
+                self.META['author'] = last_change[4]
+        except IOError:
+            # Do nothing. No meta information available, procedure may not exist
+            self.logger.debug('No META information for \"%s\".', self)
+            pass
+        except ValueError as e:
+            # Can occur if timestamp cannot be converted to datetime or if it is not found
+            raise KbError(e)
+
+    def __str__(self):
+        return self.namespace
+
+    def __repr__(self):
+        return "{} <{}>".format(self.__class__.__name__, self)
+
+    def __cmp__(self, other):
+        if isinstance(other, str):
+            if other == self.namespace: return 0
+            else: return -1
+        elif isinstance(other, Kb):
+            if self.namespace == other.namespace: return 0
         else:
-            kb_info['created'] = False
+            raise NotImplementedError("Comparing %s with is not implemented !" % type(other))
 
-        kb_details.append(kb_info)
+    def __hash__(self):
+        return hash(self.namespace)
 
-    return kb_details
 
-def iterpages():
+class Wiki(object):
     """
-    Method generator that walktrough dokuwiki pages directory and returns page full namespace.
-
-    :return: namespace (eg. ``app:metis:prod:eal``).
-    :rtype: str, unicode
+    Class that provides access to the content stored in Dokuwiki.
+    It is aware of all KB in it, you are able to search a KB, have details on it, etc...
     """
-    import logging
-    logger = logging.getLogger('kb.wiki.iterpages')
+    logger = logging.getLogger('optools.debug.kb.wiki.Wiki')
 
-    logger.debug('Pages directory is \"%s\".' % DOKUWIKI_DIR)
-    for root, lsdirs, lsfiles in os.walk(DOKUWIKI_DIR):
-        lsfiles.sort()
-        lsdirs.sort()
-        for page in lsfiles:
-            if re.search('.*\.txt$', page):
+    def __init__(self):
+        # Atrributes
+        self._index = []
+
+        # Build index and parents relations
+        self._build_index()
+        self._build_parent_relations()
+
+        # Log
+        self.logger.debug('%s attributes:\n%s', repr(self), pformat(self.__dict__))
+        self.logger.info('Indexed %d procedures.', len(self._index))
+
+    def _build_index(self):
+        """Build the index of all KB in dokuwiki (txt files in data/pages folder)."""
+        self.logger.info('Building full index for the wiki.')
+        self.logger.debug('Pages directory is \"%s\".' % DOKUWIKI_PAGES_DIR)
+
+        # Check all procedure files that exist in Dokuwiki, create Kb objects
+        for root, lsdirs, lsfiles in os.walk(DOKUWIKI_PAGES_DIR):
+            lsfiles.sort()
+            lsdirs.sort()
+
+            root_namespace = os.path.relpath(root, DOKUWIKI_PAGES_DIR).replace('/', ':')
+            if root_namespace != ".":
+                self._index.append(Kb(root_namespace))
+
+            for page in lsfiles:
                 name = os.path.splitext(page)[0]
                 path = os.path.join(root, name)
-                page = os.path.relpath(path, DOKUWIKI_DIR).replace('/', ':')
-                yield page
+                namespace = os.path.relpath(path, DOKUWIKI_PAGES_DIR).replace('/', ':')
+                self._index.append(Kb(namespace))
 
-# Module testing
-if __name__ == '__main__':
-    import logging
-    from pprint import pformat
+    def _build_parent_relations(self):
+        """Build the parents relation-ships for all Kb."""
+        self.logger.info('--- Building parents relation-ships for all Kb.')
+        for kb in self._index:
+            self.logger.debug('Building parents for \"%s\".', kb)
+            for pi, parent in enumerate(kb.parents):
+                kb_index = self._index.index(parent)
+                kb.parents[pi] = self._index[kb_index]
 
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s (<%(funcName)s>)")
-    logger = logging.getLogger('kb.wiki')
+    def __str__(self):
+        return self.__class__.__name__
 
-    # Test procedure details
-    logger.info('Testing function: get_procedure_details()')
-    page = 'app:metis:prod:oss:tata'
-    logger.debug(pformat(get_procedure_details(page)))
+    def __repr__(self):
+        return "<{}>".format(self)
 
-    # Test generator
-    logger.info('Testing generator: iterpages()')
-    for page in iterpages():
-        logger.debug(page)
+    def __getitem__(self, key):
+        if key in self._index:
+            return self._index[self._index.index(key)]
