a37b734877f8d0fa6a5dd58bd4ac67ccc197ae13
Vincent BESANCON <besancon.vincent@gmail.com>
2012-11-07 18:05:39 +0100

    Kb: Procedure model: added more columns.

--- apps/kb/models.py
+++ apps/kb/models.py
@@ -1,24 +1,15 @@
 """
-:mod:`kb.models` -- Models for app kb.
+:mod:`apps.kb.models` -- Models for app kb.
 """
-
-# Std imports
-from datetime import datetime
 
 # Django imports
 from django.db import models
-from django.utils.timezone import utc
 
 
 class Procedure(models.Model):
     """
     Procedure Model.
-
     Store informations about KB in Dokuwiki.
-
-    - Namespace
-    - Rating (grade from -2 to 1)
-    - Comment
     """
     class Meta:
         permissions = (
@@ -33,10 +24,13 @@ class Procedure(models.Model):
     )
 
     namespace = models.CharField(max_length=255, unique=True, help_text='Namespace for this procedure.')
-    rating = models.IntegerField(choices=RATING_CHOICES, default=-3)
+    rating = models.IntegerField(choices=RATING_CHOICES, default=-1)
     comment = models.TextField(help_text='Comment on the grade you given.', default="No comment")
+    is_written = models.BooleanField(help_text='Is the procedure has been written ?', default=False)
     validated = models.BooleanField(help_text='Is the procedure is validated ?', default=False)
-    last_modified = models.DateTimeField(default=datetime.now(tz=utc))
+    author = models.CharField(max_length=128, help_text='Last modification author.', blank=True, null=True)
+    parents = models.ManyToManyField('Procedure', blank=True, null=True)
+    last_modified = models.DateTimeField(blank=True, null=True)
 
     def __unicode__(self):
         return self.namespace
