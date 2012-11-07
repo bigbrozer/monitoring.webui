20cb1fed37c835e6ed6c83fc7c524fa7b781dd8d
Vincent BESANCON <besancon.vincent@gmail.com>
2012-11-07 16:07:46 +0100

    Tools: get_kb_pages.sh now download meta data.

--- tools/get_kb_pages.sh
+++ tools/get_kb_pages.sh
@@ -3,3 +3,4 @@
 PROGPATH=$(dirname $0)
 
 rsync -avz --delete monitoring-dc.app.corp:/var/www/kb/data/pages "${PROGPATH}/../var"
+rsync -avz --delete monitoring-dc.app.corp:/var/www/kb/data/meta "${PROGPATH}/../var"
