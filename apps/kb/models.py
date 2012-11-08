f129f9c79452e918a64ca36e3352d610ff35f732
Vincent BESANCON <besancon.vincent@gmail.com>
2012-11-08 14:02:37 +0100

    [kb.models.Procedure] Added some comments and __hash__() for tests on membership.

--- apps/kb/models.py
+++ apps/kb/models.py
@@ -9,13 +9,14 @@ from django.db import models
 class Procedure(models.Model):
     """
     Procedure Model.
-    Store informations about KB in Dokuwiki.
+    Store informations about KB from Dokuwiki to a database.
     """
     class Meta:
         permissions = (
             ("rate_procedure", "Can rate a procedure"),
         )
 
+    # Possible rating choices
     RATING_CHOICES = (
         (-2, 'Bad'),
         (-1, 'Average'),
@@ -23,14 +24,19 @@ class Procedure(models.Model):
         ( 1, 'Excellent'),
     )
 
+    # Table columns
     namespace = models.CharField(max_length=255, unique=True, help_text='Namespace for this procedure.')
-    rating = models.IntegerField(choices=RATING_CHOICES, default=-1)
-    comment = models.TextField(help_text='Comment on the grade you given.', default="No comment")
     is_written = models.BooleanField(help_text='Is the procedure has been written ?', default=False)
     validated = models.BooleanField(help_text='Is the procedure is validated ?', default=False)
     author = models.CharField(max_length=128, help_text='Last modification author.', blank=True, null=True)
     parents = models.ManyToManyField('Procedure', blank=True, null=True)
+    rating = models.IntegerField(choices=RATING_CHOICES, default=-1)
+    comment = models.TextField(help_text='Comment on the grade you given.', blank=True, null=True)
     last_modified = models.DateTimeField(blank=True, null=True)
 
+    # Instance representation as string
     def __unicode__(self):
         return self.namespace
+
+    def __hash__(self):
+        return hash(self.namespace)
