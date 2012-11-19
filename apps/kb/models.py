51d8f14e62e09aa347baa1354c37250ebc542630
Vincent BESANCON <besancon.vincent@gmail.com>
2012-11-19 01:48:26 +0100

    [kb] Procedure model: added title and divider to RATING_CHOICES.

--- apps/kb/models.py
+++ apps/kb/models.py
@@ -47,6 +47,8 @@ class Procedure(models.Model):
 
     # Possible rating choices
     RATING_CHOICES = (
+        ('', 'Please select rating'),
+        ('', '--------------------'),
         (-2, 'Bad'),
         (-1, 'Average'),
         ( 0, 'Good'),
