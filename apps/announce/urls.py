83dd5258781ae42a3c2f30096e9df38dd2d72b2d
Vincent BESANCON <besancon.vincent@gmail.com>
2012-11-20 11:53:03 +0100

    [announce] allow to read any announcement if you know its ID with URL: /announce/show/<ID>.

--- apps/announce/urls.py
+++ apps/announce/urls.py
@@ -4,5 +4,8 @@ urlpatterns = patterns('apps.announce.views',
     url(r'^show/$',
         view='show',
         name='announce_show'),
+    url(r'^show/(?P<announce_id>\d+)$',
+        view='show',
+        name='announce_show_id'),
 )
 
--- apps/announce/views.py
+++ apps/announce/views.py
@@ -10,10 +10,12 @@ from django.template import RequestContext
 from models import Announcement
 
 
-def show(request):
+def show(request, announce_id=None):
     """
     Show the currently active announcement.
 
+    Arguments:
+        announce_id: ID of an existing announcement to show it.
     Parameters:
         redirect: url where to redirect
     Template:
@@ -26,7 +28,11 @@ def show(request):
     except KeyError:
         redirect_url = "/"
 
-    announce = Announcement.objects.get(is_enabled=True)
+    if announce_id:
+        announce = Announcement.objects.get(pk=announce_id)
+    else:
+        announce = Announcement.objects.get(is_enabled=True)
+
     return render_to_response(
         "announce/announce_show.html",
         locals(),
